4c4a6120060f02758463c016049316e36f53df85
danmv24 <danmn119@gmail.com>
2022-09-20 21:02:12 +0300

    Author model created

---
+++ src/main/java/com/example/ElectronicLibrary/entity/Author.java
@@ -0,0 +1,19 @@
+package com.example.ElectronicLibrary.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "author")
+@Data
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+}
