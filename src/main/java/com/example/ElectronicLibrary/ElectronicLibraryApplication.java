51e7f584c7dc914570dea8480722aba3e8ab0432
danmv24 <danmn119@gmail.com>
2022-09-20 17:25:04 +0300

    first commit

---
+++ src/main/java/com/example/ElectronicLibrary/ElectronicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElectronicLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ElectronicLibrary/ElectronicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElectronicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
