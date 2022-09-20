077be4ff67ba9c07cbb6b3896d722ddf7285c804
danmv24 <danmn119@gmail.com>
2022-09-20 18:13:01 +0300

    changes in HomeController

--- src/main/java/com/example/ElectronicLibrary/controller/HomeController.java
+++ src/main/java/com/example/ElectronicLibrary/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.ElectronicLibrary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     @GetMapping
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("title", "Главная страница");
         return "home";
     }
 }
