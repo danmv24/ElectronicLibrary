fc5a8eb7b4b6abfdb7af783cb1619ae45cc7d5d0
danmv24 <danmn119@gmail.com>
2022-09-20 20:04:39 +0300

    changes in BookController

--- src/main/java/com/example/ElectronicLibrary/controller/BookController.java
+++ src/main/java/com/example/ElectronicLibrary/controller/BookController.java
@@ -1,12 +1,14 @@
 package com.example.ElectronicLibrary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
     @GetMapping("/addbook")
-    public String getAddBookPage() {
+    public String getAddBookPage(Model model) {
+        model.addAttribute("title", "Add Book");
         return "addbook";
     }
 }
