47ac99aff729467ab8fe96c11008650e1126753e
danmv24 <danmn119@gmail.com>
2022-09-21 17:51:51 +0300

    the addBook method has been created in the BookController, which processes Post requests for entering the book into the database

--- src/main/java/com/example/ElectronicLibrary/controller/BookController.java
+++ src/main/java/com/example/ElectronicLibrary/controller/BookController.java
@@ -3,6 +3,10 @@ package com.example.ElectronicLibrary.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
 
 @Controller
 public class BookController {
@@ -10,5 +14,10 @@ public class BookController {
     public String getAddBookPage(Model model) {
         model.addAttribute("title", "Add Book");
         return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@RequestParam String name, @RequestParam String title, @RequestParam File file) {
+
     }
 }
