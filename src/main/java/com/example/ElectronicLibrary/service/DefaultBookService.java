29456ce46fcb4814b7b19997d8017bf0b91539a2
danmv24 <danmn119@gmail.com>
2022-09-27 17:48:53 +0300

    changes in DefaultBookService

--- src/main/java/com/example/ElectronicLibrary/service/DefaultBookService.java
+++ src/main/java/com/example/ElectronicLibrary/service/DefaultBookService.java
@@ -2,6 +2,7 @@ package com.example.ElectronicLibrary.service;
 
 import com.example.ElectronicLibrary.entity.BookEntity;
 import com.example.ElectronicLibrary.form.BookForm;
+import com.example.ElectronicLibrary.repository.AuthorRepository;
 import com.example.ElectronicLibrary.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class DefaultBookService implements BookService{
 
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
 
     @Autowired
-    public DefaultBookService(BookRepository bookRepository) {
+    public DefaultBookService(BookRepository bookRepository, AuthorRepository authorRepository) {
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
     }
 
     @Override
@@ -22,5 +25,6 @@ public class DefaultBookService implements BookService{
         BookEntity bookEntity = new BookEntity();
         bookEntity.setTitle(bookForm.getTitle());
         bookEntity.setDescription(bookForm.getDescription());
+        authorRepository.findByNameAndSurname(bookForm.getAuthor().getName(), bookForm.getAuthor().getSurname());
     }
 }
