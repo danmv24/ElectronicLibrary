7404d331dd3568f5c8ce66e26550584802beeb4c
danmv24 <danmn119@gmail.com>
2022-09-27 16:29:48 +0300

    partially implemented the save method in the DefaultBookService class

--- src/main/java/com/example/ElectronicLibrary/service/DefaultBookService.java
+++ src/main/java/com/example/ElectronicLibrary/service/DefaultBookService.java
@@ -1,13 +1,25 @@
 package com.example.ElectronicLibrary.service;
 
+import com.example.ElectronicLibrary.entity.BookEntity;
+import com.example.ElectronicLibrary.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class DefaultBookService implements BookService{
 
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public DefaultBookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public void save(BookForm bookForm, MultipartFile file) {
-
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setTitle(bookForm.getTitle());
+        bookEntity.setDescription(bookForm.getDescription());
     }
 }
