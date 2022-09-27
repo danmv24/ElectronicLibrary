6e2a1adb988144f41bb1768f652bc612125acdf6
danmv24 <danmn119@gmail.com>
2022-09-27 17:49:55 +0300

    changes in repositories

--- src/main/java/com/example/ElectronicLibrary/repository/AuthorRepository.java
+++ src/main/java/com/example/ElectronicLibrary/repository/AuthorRepository.java
@@ -2,9 +2,11 @@ package com.example.ElectronicLibrary.repository;
 
 import com.example.ElectronicLibrary.entity.AuthorEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AuthorRepository extends CrudRepository<AuthorEntity, Long> {
     Optional<AuthorEntity> findByNameAndSurname(String name, String surname);
 }
--- src/main/java/com/example/ElectronicLibrary/repository/BookRepository.java
+++ src/main/java/com/example/ElectronicLibrary/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.example.ElectronicLibrary.repository;
 
 import com.example.ElectronicLibrary.entity.BookEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<BookEntity, Long> {
 }
