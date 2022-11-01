57b90061f3bb49581aeb10b10cde1ae8bdbc3575
danmv24 <danmn119@gmail.com>
2022-11-01 13:37:01 +0300

    changes generation type for id in entities

--- src/main/java/com/example/ElectronicLibrary/entity/AuthorEntity.java
+++ src/main/java/com/example/ElectronicLibrary/entity/AuthorEntity.java
@@ -14,7 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 public class AuthorEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
