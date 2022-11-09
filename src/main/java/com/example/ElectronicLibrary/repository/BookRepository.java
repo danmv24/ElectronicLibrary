a7d77c41ff33ce0165917167cc597ba8115ba130
danmv24 <danmn119@gmail.com>
2022-11-09 21:27:53 +0300

    override method findAll

--- src/main/java/com/example/ElectronicLibrary/repository/BookRepository.java
+++ src/main/java/com/example/ElectronicLibrary/repository/BookRepository.java
@@ -4,8 +4,10 @@ import com.example.ElectronicLibrary.entity.BookEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface BookRepository extends CrudRepository<BookEntity, Long> {
-
+    List<BookEntity> findAll();
 }
