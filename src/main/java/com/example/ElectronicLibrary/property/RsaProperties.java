779b59efe7293aa2df411d9280530651490f0252
danmv24 <danmn119@gmail.com>
2022-11-22 18:13:30 +0300

    access to keys from application.properties

---
+++ src/main/java/com/example/ElectronicLibrary/property/RsaProperties.java
@@ -0,0 +1,16 @@
+package com.example.ElectronicLibrary.property;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public class RsaProperties {
+    @Value("${rsa.private-key}")
+    private RSAPrivateKey privateKey;
+
+    @Value("${rsa.public-key}")
+    private RSAPublicKey publicKey;
+}
