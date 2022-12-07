bb238c2b2e1218981fe67544ede37a6f9f0618dd
danmv24 <danmn119@gmail.com>
2022-12-07 09:27:08 +0300

    add @repository annotation

--- src/main/java/com/example/ElectronicLibrary/repository/UserRepository.java
+++ src/main/java/com/example/ElectronicLibrary/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.ElectronicLibrary.repository;
 
 import com.example.ElectronicLibrary.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 }
