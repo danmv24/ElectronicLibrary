e85d9b62ace305a9d481e262f51b55bde2d6b599
danmv24 <danmn119@gmail.com>
2022-12-07 09:30:10 +0300

    logic for user registration, user login to your account by username and password

--- src/main/java/com/example/ElectronicLibrary/service/impl/DefaultAuthService.java
+++ src/main/java/com/example/ElectronicLibrary/service/impl/DefaultAuthService.java
@@ -1,21 +1,30 @@
 package com.example.ElectronicLibrary.service.impl;
 
+import com.example.ElectronicLibrary.entity.UserEntity;
 import com.example.ElectronicLibrary.form.UserForm;
 import com.example.ElectronicLibrary.mapper.UserMapper;
 import com.example.ElectronicLibrary.repository.UserRepository;
 import com.example.ElectronicLibrary.service.AuthService;
+import com.example.ElectronicLibrary.service.TokenService;
+import com.example.ElectronicLibrary.view.JwtView;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
+@Service
 public class DefaultAuthService implements AuthService {
 
-//    private final TokenService tokenService;
-    private final AuthenticationManager authManager;
-//    private final DefaultUserDetailsService defaultUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
 
     private final UserRepository userRepository;
 
@@ -24,16 +33,23 @@ public class DefaultAuthService implements AuthService {
         userRepository.save(UserMapper.toEntity(userForm, passwordEncoder.encode(userForm.getPassword())));
     }
 
-//    @Override
-//    public JwtView login(UserForm userForm) {
-//        UsernamePasswordAuthenticationToken authenticationToken =
-//                new UsernamePasswordAuthenticationToken(userForm.getUsername(), userForm.getPassword());
-//        Authentication auth = authManager.authenticate(authenticationToken);
-//
-//        DefaultUserDetails user = (DefaultUserDetails) defaultUserDetailsService.loadUserByUsername(userForm.getUsername());
-//        String accessToken = tokenService.generateAccessToken(user);
-//        String refreshToken = tokenService.generateRefreshToken(user);
-//
-//        return new JwtView(accessToken, refreshToken);
-//    }
+    @Override
+    public JwtView authenticateUser(UserForm userForm) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userForm.getUsername(), userForm.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        DefaultUserDetails userDetails = (DefaultUserDetails) auth.getPrincipal();
+
+        String accessToken = tokenService.generateAccessToken(userDetails);
+        String refreshToken = tokenService.generateRefreshToken(userDetails);
+
+        return new JwtView(accessToken, refreshToken);
+    }
+
+    @Override
+    public List<UserEntity> all() {
+        return (List<UserEntity>) userRepository.findAll();
+    }
 }
