1f540d337e4b905d94db762e32f120d56294e066
danmv24 <danmn119@gmail.com>
2022-12-07 09:14:46 +0300

    create table users

--- src/main/resources/db/changelog/changes/create_table_users.sql
+++ src/main/resources/db/changelog/changes/create_table_users.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "users" (
     id BIGSERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL
 );
