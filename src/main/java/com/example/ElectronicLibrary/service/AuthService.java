7dcc61ceb5fd3b1cd0f4bd835deaff28911afbf4
danmv24 <danmn119@gmail.com>
2022-12-07 09:28:52 +0300

    interface AuthService

--- src/main/java/com/example/ElectronicLibrary/service/AuthService.java
+++ src/main/java/com/example/ElectronicLibrary/service/AuthService.java
@@ -1,9 +1,15 @@
 package com.example.ElectronicLibrary.service;
 
+import com.example.ElectronicLibrary.entity.UserEntity;
 import com.example.ElectronicLibrary.form.UserForm;
+import com.example.ElectronicLibrary.view.JwtView;
+
+import java.util.List;
 
 public interface AuthService {
 
     void create(UserForm userForm);
-//    JwtView login(UserForm userForm);
+    JwtView authenticateUser(UserForm userForm);
+
+    List<UserEntity> all();
 }
