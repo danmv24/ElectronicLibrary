e1b6d30dd3622f4aec141fd87ccf7cce9a8a9442
danmv24 <danmn119@gmail.com>
2022-12-07 09:32:58 +0300

    impl UserDetails

---
+++ src/main/java/com/example/ElectronicLibrary/service/impl/DefaultUserDetails.java
@@ -0,0 +1,65 @@
+package com.example.ElectronicLibrary.service.impl;
+
+import com.example.ElectronicLibrary.entity.UserEntity;
+import com.example.ElectronicLibrary.mapper.UserMapper;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+@Builder
+public class DefaultUserDetails implements UserDetails {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    public static DefaultUserDetails build(UserEntity user) {
+        return UserMapper.toUserDetails(user);
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
