00ec6956ec4808ffdeec9962985ed6cb5a4518d7
danmv24 <danmn119@gmail.com>
2022-12-07 09:19:59 +0300

    AuthController for registration, user login, getting a list of all users for admins

---
+++ src/main/java/com/example/ElectronicLibrary/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.ElectronicLibrary.controller;
+
+import com.example.ElectronicLibrary.entity.UserEntity;
+import com.example.ElectronicLibrary.form.UserForm;
+import com.example.ElectronicLibrary.service.AuthService;
+import com.example.ElectronicLibrary.view.JwtView;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public void signUp(@RequestPart("user") UserForm userForm) {
+        authService.create(userForm);
+    }
+
+    @PostMapping("/login")
+    public JwtView login(@RequestPart("user") UserForm userForm) {
+        return authService.authenticateUser(userForm);
+    }
+
+    @GetMapping("/all")
+    public List<UserEntity> all() {
+        return authService.all();
+    }
+
+
+}
