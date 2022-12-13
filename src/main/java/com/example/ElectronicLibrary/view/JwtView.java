de2f4322e52f8d53a4a74261bdc5e7b5ca053a86
danmv24 <danmn119@gmail.com>
2022-12-13 21:58:47 +0300

    added builder annotation

--- src/main/java/com/example/ElectronicLibrary/view/JwtView.java
+++ src/main/java/com/example/ElectronicLibrary/view/JwtView.java
@@ -1,12 +1,14 @@
 package com.example.ElectronicLibrary.view;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class JwtView {
 
     private String accessToke;
