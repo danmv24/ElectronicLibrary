be9cf4bad3b95bebe5a981296cf22a3196bf0df4
danmv24 <danmn119@gmail.com>
2023-01-03 18:57:41 +0300

    small changes

--- src/main/java/com/example/ElectronicLibrary/enums/Role.java
+++ src/main/java/com/example/ElectronicLibrary/enums/Role.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
 
     ROLE_ADMIN("ROLE_ADMIN"),
-    ROLE_USER("ROLE_ADMIN");
+    ROLE_USER("ROLE_USER");
 
     private final String value;
 
