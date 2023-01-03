13bb2e6c5820efad5d8cda4546500db1bf073628
danmv24 <danmn119@gmail.com>
2023-01-03 18:59:00 +0300

    user service

---
+++ src/main/java/com/example/ElectronicLibrary/service/impl/DefaultUserService.java
@@ -0,0 +1,39 @@
+package com.example.ElectronicLibrary.service.impl;
+
+import com.example.ElectronicLibrary.entity.UserEntity;
+import com.example.ElectronicLibrary.enums.Role;
+import com.example.ElectronicLibrary.exception.RoleNotFoundException;
+import com.example.ElectronicLibrary.exception.UserNotFoundException;
+import com.example.ElectronicLibrary.repository.UserRepository;
+import com.example.ElectronicLibrary.service.UserService;
+import liquibase.repackaged.org.apache.commons.lang3.EnumUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class DefaultUserService implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void changeRole(Long id, String roleName) {
+        Optional<UserEntity> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            throw new UserNotFoundException(HttpStatus.BAD_REQUEST, "User with id " +id+ " was not found!");
+        }
+
+        if (EnumUtils.isValidEnumIgnoreCase(Role.class, roleName)) {
+            user.get().setRole(roleName);
+            userRepository.save(user.get());
+        } else {
+            throw new RoleNotFoundException(HttpStatus.BAD_REQUEST, "Role with name " +roleName+ " was not found!");
+        }
+
+
+    }
+}
