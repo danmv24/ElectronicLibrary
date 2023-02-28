aa51a63603ccedb214d604021a84009fbb7af187
danmv24 <danmn119@gmail.com>
2023-02-28 21:38:43 +0300

    tests

---
+++ src/test/java/com/example/ElectronicLibrary/service/impl/DefaultAuthServiceTest.java
@@ -0,0 +1,106 @@
+package com.example.ElectronicLibrary.service.impl;
+
+import com.example.ElectronicLibrary.entity.UserEntity;
+import com.example.ElectronicLibrary.exception.UserAlreadyExistsException;
+import com.example.ElectronicLibrary.form.UserForm;
+import com.example.ElectronicLibrary.repository.UserRepository;
+import com.example.ElectronicLibrary.service.TokenService;
+import com.example.ElectronicLibrary.view.JwtView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class DefaultAuthServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private TokenService tokenService;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+
+    private DefaultUserDetails defaultUserDetails;
+
+    @Mock
+    private Authentication authentication;
+
+    private UserForm userForm;
+
+    @InjectMocks
+    private DefaultAuthService authService;
+
+    @BeforeEach
+    public void setUp() {
+        userForm = UserForm.builder()
+                .username("username")
+                .password("password")
+                .build();
+
+        defaultUserDetails = DefaultUserDetails.builder()
+                .id(1L)
+                .username("username")
+                .password("password")
+                .role("ROLE_USER")
+                .build();
+    }
+
+
+
+
+    @Test
+    void testCreateUserOk() {
+        when(userRepository.existsByUsername(any())).thenReturn(false);
+        when(passwordEncoder.encode(any())).thenReturn("encode_password");
+
+        authService.create(userForm);
+
+
+        verify(userRepository).save(any(UserEntity.class));
+    }
+
+    @Test
+    void testCreateUserWhenUserAlreadyExists() {
+        when(userRepository.existsByUsername(userForm.getUsername())).thenReturn(true);
+
+        assertThrows(UserAlreadyExistsException.class, () -> {
+            authService.create(userForm);
+        });
+
+        when(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userForm.getUsername(), userForm.getPassword()))).thenReturn(mock(Authentication.class));
+    }
+
+    @Test
+    void testAuthenticateUser() {
+        String accessToken = "test_access_token";
+        String refreshToken = "test_refresh_token";
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(defaultUserDetails);
+        when(tokenService.generateAccessToken(defaultUserDetails)).thenReturn(accessToken);
+        when(tokenService.generateRefreshToken(defaultUserDetails)).thenReturn(refreshToken);
+
+        JwtView jwtView = authService.authenticateUser(userForm);
+
+        assertEquals(accessToken, jwtView.getAccessToken());
+        assertEquals(refreshToken, jwtView.getRefreshToken());
+    }
+
+}
